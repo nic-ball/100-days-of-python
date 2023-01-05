11385e02f5e5658d7adbafbaf4471be1da4280f0
nic-ball <nicholasball147@gmail.com>
2023-01-05 00:49:32 +0000

    Band Name generator

---
+++ day_1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator\n")
+city = input("What Town/City did you grow up in? \n")
+pet = input("What is the name or your pet? \n")
+band_name = city + " " + pet
+print("Your new band name is: ", band_name)
---
+++ day_1/tuple_swap.py
@@ -0,0 +1,11 @@
+a = input("a: ")
+b = input("b: ")
+
+a,b = b,a  
+
+# c = a
+# a = b
+# b = c
+
+print("a: " + a)
+print("b: " + b)
