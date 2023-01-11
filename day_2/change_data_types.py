f069e7a951f99478926a1d74f4a2a4d5ea5514e4
nic-ball <nicholasball147@gmail.com>
2023-01-11 14:53:12 +0000

    Taking input and changing the data type to produce a result

---
+++ day_2/change_data_types.py
@@ -0,0 +1,6 @@
+two_digit_number = input("Type a two digit number: \n")
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+sum = int(first_number) + int(second_number)
+print(sum)
+
