dc129faa160b216944558c85b35c06b7533f30d8
nic-ball <nicholasball147@gmail.com>
2023-01-12 17:17:15 +0000

    Refactored pizza_ordering, This was to sort out some scoping issues that could have arisen with added complexity

--- day_3/pizza_ordering.py
+++ day_3/pizza_ordering.py
@@ -1,7 +1,7 @@
 print("Welcome to Python Pizza!")
 size = input("What size pizza do you want? S, M, or L?: \n")
-add_pepperoni = input("Do you want pepperoni?: Y or N \n")
-extra_cheese = input("Do you want extra cheese?: Y or N \n")
+add_pepperoni = input("Do you want pepperoni?: Y or N? \n")
+extra_cheese = input("Do you want extra cheese?: Y or N? \n")
 
 bill = 0
 
