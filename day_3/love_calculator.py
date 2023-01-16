11455e53e980bc29b5fa5236035afbb9cfc59c91
nic-ball <nicholasball147@gmail.com>
2023-01-16 17:19:49 +0000

    Created love_calculator, Takes 2 full names, counts how many times certain letters appear in each name, gives a score

---
+++ day_3/love_calculator.py
@@ -0,0 +1,30 @@
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+name1 = name1.lower()
+name2 = name2.lower()
+
+t = name1.count("t") + name2.count("t")
+r = name1.count("r") + name2.count("r")
+u = name1.count("u") + name2.count("u")
+e = name1.count("e") + name2.count("e")
+
+l = name1.count("l") + name2.count("l")
+o = name1.count("o") + name2.count("o")
+v = name1.count("v") + name2.count("v")
+
+
+score_1 = t + r + u + e
+score_2 = l + o + v + e
+
+love_score = str(score_1) + str(score_2)
+love_score = int(love_score)
+
+if (love_score < 10) or (love_score > 90):
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
+    
