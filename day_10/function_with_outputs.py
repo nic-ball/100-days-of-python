a40b13d4077a72a8bc3c820b67fafbd7349d9a95
nic-ball <nicholasball147@gmail.com>
2023-08-15 13:53:13 +0100

    Added a catch statement incase no inputs were provided

--- day_10/function_with_outputs.py
+++ day_10/function_with_outputs.py
@@ -5,6 +5,8 @@ l_name = input("What is your last name?: ")
 
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     return f"Welcome {formatted_f_name} {formatted_l_name}!"
