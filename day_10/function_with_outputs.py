770ad068accf845175ca6fe9852ea4ce2e55b10e
nic-ball <nicholasball147@gmail.com>
2023-08-15 13:18:00 +0100

    Functions with outputs

---
+++ day_10/function_with_outputs.py
@@ -0,0 +1,13 @@
+# Functions with Outputs
+
+f_name = input("What is your first name?: ")
+l_name = input("What is your last name?: ")
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"Welcome {formatted_f_name} {formatted_l_name}!"
+
+formatted_string = format_name(f_name, l_name)
+print(formatted_string)
